b318ab2b755de0dc2ebd940fa8a6f6cbe1c3736f
Ankur Sharma <ankurs4@cisco.com>
2022-04-19 09:07:29 +0200

    add rio options and hunit

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import qualified MyLib (someFunc)
+import Prelude
 
 main :: IO ()
 main = do
--- test/MyLibTest.hs
+++ test/MyLibTest.hs
@@ -1,4 +1,19 @@
 module Main (main) where
 
+import Test.HUnit
+import Prelude
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+    putStrLn "Running test suite"
+    (_counts :: Counts) <- runTestTT tests
+    putStrLn $ "Test counts summary: " ++ showCounts _counts
+
+tests :: Test
+tests = TestList [test1]
+
+test1 :: Test
+test1 = TestCase $
+    assertEqual "asserting values are same"
+    (89::Integer)
+    (90::Integer)
